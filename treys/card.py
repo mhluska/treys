c856e89eab936a6e0e9f82da3ea4d675d63a43ca
Kimi Heinonen <kimi.heinonen@gmail.com>
2023-03-08 22:31:50 +0200

    Fix 104 card deck

--- treys/card.py
+++ treys/card.py
@@ -29,6 +29,7 @@ class Card:
 
     # the basics
     STR_RANKS: str = '23456789TJQKA'
+    STR_SUITS: str = 'shdc'
     INT_RANKS: range = range(13)
     PRIMES: list[int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
 
